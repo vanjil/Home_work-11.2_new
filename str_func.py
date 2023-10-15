db52d49e0d4fcf5779165a1ee748cffc3cc18f87
Vladimir Nikitin <vanjilbroolkyn@gmail.com>
2023-10-15 14:05:42 -0400

    Добавлена функция и докстринги

--- str_func.py
+++ str_func.py
@@ -1,2 +1,10 @@
 def capitalize_string(input_string):
     return input_string.upper()
+def capitalize_string(input_string):
+    """
+    Эта функция принимает входную строку и возвращает ее в виде строки с заглавными буквами.
+
+    :param input_string: Входная строка, которую нужно преобразовать в заглавные буквы.
+    :return: Входная строка в виде строки с заглавными буквами.
+    """
+    return input_string.upper()
